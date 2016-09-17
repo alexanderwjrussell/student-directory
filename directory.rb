7230e04b6cdad84b463c165f83907fedcb70b484
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-17 22:52:01 +0100

    Exercise 2 complete. Added default filename to try_laod_students method

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
+@students = []
+
 def input_students
-  @students = []
   puts "Please enter the names of the students"
   # Keep adding students until the break criteria is hit
   loop do
@@ -117,8 +118,9 @@ end
 
 def try_load_students
   filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
+  if filename.nil?
+    then load_students
+  elsif File.exists?(filename)
     load_students(filename)
     puts "Loaded #{@students.count} from #{filename}"
   else
