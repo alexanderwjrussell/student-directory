1184ecb319aa200153a33fabfc5be350c18e6c9d
alexanderwjrussell <awjrussell@live.co.uk>
2016-09-17 20:20:10 +0100

    Section 10 complete. Interactive menu refactored and working.

--- directory.rb
+++ directory.rb
@@ -1,10 +1,10 @@
 def input_students
-  students = []
+  @students = []
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # Keep adding students until the break criteria is hit
   loop do
-    #Loop through the questions
+    # Loop through the questions
     puts "Students name: "
     name = gets.delete "\n"
       if name.empty?
@@ -21,18 +21,18 @@ def input_students
         hobby = "None"
       end
     # Add the student information to the hashes in the array
-    students << {name: name, cohort: cohort, hobby: hobby}
-    if students.count == 1
-      puts "Now we have #{students.count} student"
+    @students << {name: name, cohort: cohort, hobby: hobby}
+    if @students.count == 1
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     puts ""
     puts "Add another student (Y/N)"
     break if gets.chomp.downcase == "n"
   end
 # Return the array of students
-students
+@students
 end
 
 def print_header
@@ -40,9 +40,9 @@ def print_header
   puts "--------------".center(40)
 end
 
-def print(students)
+def print_students_list(students)
   count = 0
-    until count == students.length
+    until count == @students.length
       puts "#{count + 1}. #{students[count][:name]} (#{students[count][:cohort]} cohort)".center(40)
       count += 1
     end
@@ -54,42 +54,50 @@ end
 
 def name_letter_sort(students)
   letter = "D"
-  select_students = students.select{|student| student[:name][0] == letter}
+  select_students = @students.select{|student| student[:name][0] == letter}
   puts select_students
 end
 
 def lengthy_name_sort(students)
   length = 12
-  select_students = students.select{|student| student[:name].length < length}
+  select_students = @students.select{|student| student[:name].length < length}
   puts select_students
 end
 
 def sort_cohort(students)
-  students.sort_by{|student| student[:cohort]}
+  @students.sort_by{|student| student[:cohort]}
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list(@students)
+  print_footer(@students)
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def interactive_menu
-  students = input_students
+  @students = input_students
   loop do
-    # Print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # Read the input and save it into a variable
-    selection = gets.chomp
-    # Do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
